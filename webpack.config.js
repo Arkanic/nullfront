913d5095d5c74924d721bac9a2e4d5da48ceda90
Arkanic <silverfishasaurus@gmail.com>
2023-11-29 18:48:42 +0000

    build template

---
+++ webpack.config.js
@@ -0,0 +1,111 @@
+const path = require("path");
+
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+    mode: "production",
+
+    stats: {
+        children: true
+    },
+
+    entry: {
+        index: "./src/client/index.ts"
+    },
+    output: {
+        filename: "[contenthash].js",
+        path: path.resolve(__dirname, "dist")
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        },
+        moduleIds: "deterministic",
+        minimizer: [
+            new TerserJSPlugin({})
+        ]
+    },
+    resolve: {
+        extensions: [
+            ".ts", ".tsx",
+            ".js",
+            ".css", ".scss"
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-modules-typescript-loader"
+                    },
+                    {
+                        loader: MiniCSSExtractPlugin.loader,
+                        options: {
+                            esModule: false
+                        }
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: false,
+                            sourceMap: false
+                        }
+                    },
+                    {
+                        loader: "sass-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: "ts-loader"
+            },
+            {
+                test: /\.pug$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                exclude: /node_modules/,
+                type: "asset/resource",
+            }
+        ]
+    },
+    
+    plugins: [
+        /*new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: "src/client/assets/icons/",
+                    to: ""
+                }
+            ]
+        }),*/
+        new MiniCSSExtractPlugin({
+            filename: "[contenthash].css"
+        }),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "src/client/templates/index.html",
+            chunks: ["index"]
+        }),
+        new CleanWebpackPlugin()
+    ]
+}
